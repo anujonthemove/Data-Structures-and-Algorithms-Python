609cbbaae189ea3988c4b3395b0420fce62909a0
Anuj Khandelwal <anujonthemove@gmail.com>
2022-09-03 15:05:11 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup, find_packages
 
 
-setup(name='Data-Structures-and-Algorithms-Python', version='1.0', packages=find_packages())
+setup(
+    name="Data-Structures-and-Algorithms-Python",
+    version="1.0",
+    packages=find_packages(),
+)
