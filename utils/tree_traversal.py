d0081ad69c3b41c57028cafd950a21e1d6acd184
Anuj Khandelwal <anujonthemove@gmail.com>
2022-09-17 16:00:16 +0530

    level order or breadth first traversal

--- utils/tree_traversal.py
+++ utils/tree_traversal.py
@@ -1,3 +1,16 @@
+from utils.tree_properties import get_height
+
+
+def print_elements_at_level(root, level):
+    if root is None:
+        return
+    if level == 1:
+        print(root.data)
+    else:
+        print_elements_at_level(root.left, level - 1)
+        print_elements_at_level(root.right, level - 1)
+
+
 class TreeTraversal:
     @staticmethod
     def traverse_preorder(root):
@@ -50,3 +63,35 @@ class TreeTraversal:
 
         if root:
             print(root.data)
+
+    @staticmethod
+    def print_level_order_traversal_recursive(root):
+        height = get_height(root)
+        for i in range(1, height + 1):
+            print_elements_at_level(root, i)
+
+    @staticmethod
+    def level_order_traversal_iterative(root):
+
+        if root is None:
+            print("Cannot traverse an empty tree")
+            return
+
+        bfs_elements = []
+        queue = []
+
+        # insert first element or root into the queue
+        queue.append(root)
+
+        while len(queue) > 0:
+
+            # pop the element from the front of the queue
+            node = queue.pop(0)
+            bfs_elements.append(node.data)
+
+            if node.left is not None:
+                queue.append(node.left)
+
+            if node.right is not None:
+                queue.append(node.right)
+        return bfs_elements
