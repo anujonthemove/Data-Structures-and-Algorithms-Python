483e46d6df4eb267951fcf451e3b9a630a679d44
Anuj Khandelwal <anujonthemove@gmail.com>
2022-09-19 10:56:31 +0530

    get height concise, balanced binary tree

--- utils/tree_properties.py
+++ utils/tree_properties.py
@@ -34,3 +34,62 @@ def get_height(root):
         return left_height + 1
     else:
         return right_height + 1
+
+
+def get_height_concise(root):
+
+    if root is None:
+        return 0
+
+    return max(get_height_concise(root.left), get_height_concise(root.right)) + 1
+
+
+def is_balanced_naive(root):
+    if root is None:
+        return True
+
+    # calculate the height of both
+    # left subtree and right subtree of the current node
+    left_height = get_height_concise(root.left)
+    right_height = get_height_concise(root.right)
+
+    # check if the difference between left height and right height
+    # is less than or equal to 1.
+    # and then recursively check for left and right subtree
+    if (
+        (abs(left_height - right_height) <= 1)
+        and is_balanced_naive(root.left)
+        and is_balanced_naive(root.right)
+    ):
+        return True
+    return False
+
+
+def is_balanced_optimized(root):
+    # while calculating height only we check
+    # whether the tree is balanced or not
+    if root is None:
+        return True
+
+    left_height = is_balanced_optimized(root.left)
+
+    if left_height == -1:
+        return -1
+
+    right_height = is_balanced_optimized(root.right)
+
+    if right_height == -1:
+        return -1
+
+    if abs(left_height - right_height) > 1:
+        return -1
+    else:
+        return max(left_height, right_height) + 1
+
+
+def is_balanced_optimized_driver(root):
+    check_height = is_balanced_optimized(root)
+    if check_height == -1:
+        print("Tree is not balanced")
+    else:
+        print("Tree is balanced with height: {}".format(check_height))
