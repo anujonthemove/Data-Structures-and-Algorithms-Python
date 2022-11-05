32c4a75e5269a12e6d99956ddbeb8f131bfaa802
Anuj Khandelwal <anujonthemove@gmail.com>
2022-11-05 13:47:39 +0530

    added setup.bat

---
+++ setup.bat
@@ -0,0 +1,32 @@
+@echo --------------------------------------------------------------------------------
+@echo           Setting up Python Virtual Environment                        
+@echo --------------------------------------------------------------------------------
+
+@IF EXIST .env-template (
+    IF NOT EXIST .env (
+        COPY .env-template .env
+    )
+)
+
+@IF NOT EXIST .venv (
+    python -m venv .venv
+)
+
+CALL .venv\Scripts\activate
+
+python -m pip install --upgrade pip
+pip install --no-cache-dir pipenv
+
+@IF "%1" == "--dev" (
+    pipenv install --dev
+
+    @REM @IF NOT EXIST .secrets.baseline (
+    @REM     detect-secrets scan >.secrets.baseline
+    @REM )
+    @REM pre-commit install
+
+) 
+
+ELSE (
+    pipenv install
+)
