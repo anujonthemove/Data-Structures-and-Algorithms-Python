8d7a731c1885483482082d9e6a5b25bac0d82eb3
Anuj Khandelwal <anujonthemove@gmail.com>
2022-11-10 07:50:24 +0530

    added class for weighted graphs

--- utils/graph.py
+++ utils/graph.py
@@ -2,9 +2,37 @@ from collections import defaultdict
 
 
 class GraphAdjList:
+    """
+    unweighted graph
+    """
+
     def __init__(self) -> None:
         # dictionary of list
         self.graph = defaultdict(list)
 
-    def insert_node(self, vertex, data):
-        self.graph[vertex].append(data)
+    # In this representation, one needs to manually add the forward and backward node
+
+    def insert_node(self, u, v):
+        self.graph[u].append(v)
+
+    def insert_node_bothways(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+
+# class WeightedGraphAdjList:
+
+#     def __init__(self):
+#         self.graph = defaultdict(dict)
+
+#     def insert_weighted_node(self, u, v, w):
+#         self.graph[u][v] = w
+
+
+class WeightedGraphAdjList:
+    def __init__(self):
+        # self.num_vertices = num_vertices
+        self.graph = []
+
+    def insert_weighted_node(self, u, v, w):
+        self.graph.append([u, v, w])
