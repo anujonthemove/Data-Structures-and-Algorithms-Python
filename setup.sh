364e296b0720bffabaa7b9f4428b21e735b66b8f
Anuj Khandelwal <anujonthemove@gmail.com>
2022-12-20 20:06:03 +0530

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -19,16 +19,6 @@ fi
 # activate the newly created virtual environment
 source .venv/bin/activate
 
-# Test this first before uncommenting
-# python -m pip config --site set install.user false
-# python -m pip config --site set global.disable-pip-version-check false
-
-# uncomment the code below, in case you trying to access pypi through a gateway behind a company firewall
-
-# export $(grep -v '^#' .env | xargs)
-#python -m pip install --upgrade pip -i https://$JFROG_USERNAME:$JFROG_API_KEY@amat-jfrog.amat.com/artifactory/api/$JFROG_REPOSITORY
-#pip install pipenv -i https://$JFROG_USERNAME:$JFROG_API_KEY@amat-jfrog.amat.com/artifactory/api/$JFROG_REPOSITORY
-
 # upgrade .venv python-pip
 python -m pip install --upgrade pip
 
@@ -42,7 +32,7 @@ if [[ "$1" == "--dev" ]]; then
 	# if [ ! -f ".secrets.baseline" ]; then
 	# 	detect-secrets scan >.secrets.baseline
 	# fi
-	# pre-commit install
+	pre-commit install
 
 else
 	pipenv install
